381c07768c162f8723ccbbd26367f8a9b68d99fa
fuhuan1991 <huanfu2019@gmail.com>
2020-02-25 11:23:51 -0500

    add static materials

---
+++ src/main/resources/static/precache-manifest.d2547e07f0f888a62fbd4794886f8f7d.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a6b71980355eec3d69cbdc62b1ac4f3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c347b3fad67b4aff25c9",
+    "url": "/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "25ec54c3b5e05e28d784",
+    "url": "/static/css/main.14aaf0af.chunk.css"
+  },
+  {
+    "revision": "c347b3fad67b4aff25c9",
+    "url": "/static/js/2.8d024c34.chunk.js"
+  },
+  {
+    "revision": "2b993ddac0da262858e19ec8ca8bb46f",
+    "url": "/static/js/2.8d024c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25ec54c3b5e05e28d784",
+    "url": "/static/js/main.9233e936.chunk.js"
+  },
+  {
+    "revision": "a0f011b34eb2f352d77b",
+    "url": "/static/js/runtime-main.503500fc.js"
+  },
+  {
+    "revision": "a9a8c658555a30e0365cdec7b4e5765c",
+    "url": "/static/media/chalk_font.a9a8c658.ttf"
+  },
+  {
+    "revision": "f4a751e8f3dfcbf185e332c67e789f0b",
+    "url": "/static/media/chalk_font.f4a751e8.woff"
+  },
+  {
+    "revision": "eccb301b25d82e87ff79dfbddd9f053e",
+    "url": "/static/media/dashboard.eccb301b.png"
+  },
+  {
+    "revision": "7dc3208c7b19be6fd6efab10d92ec795",
+    "url": "/static/media/data_management.7dc3208c.png"
+  },
+  {
+    "revision": "9d066c1f6d6087e98f6cee01b9b1417b",
+    "url": "/static/media/graph.9d066c1f.png"
+  },
+  {
+    "revision": "d583ef956fc3e6547784fe5c5f3a20d3",
+    "url": "/static/media/graph2.d583ef95.png"
+  },
+  {
+    "revision": "6fece29beb44f127ef1c0dd6828e4761",
+    "url": "/static/media/h.6fece29b.png"
+  },
+  {
+    "revision": "e8315dc0b2560aebecd92aaaf1b3b411",
+    "url": "/static/media/home_page.e8315dc0.png"
+  },
+  {
+    "revision": "457cc00d385f2493561bbda5c3650233",
+    "url": "/static/media/t.457cc00d.png"
+  },
+  {
+    "revision": "53cd660d4a99d7add008354d13308358",
+    "url": "/static/media/wood.53cd660d.jpg"
+  }
+]);
