f8c51b8e8cde0345fc9681b686ddb3e7d4b14d2a
fuhuan1991 <huanfu2019@gmail.com>
2020-02-26 23:08:58 -0500

    JWT test

--- src/main/java/com/winterproject/demo/user/UserController.java
+++ src/main/java/com/winterproject/demo/user/UserController.java
@@ -1,7 +1,17 @@
 package com.winterproject.demo.user;
 
+import com.winterproject.demo.AuthenticationRequest;
+import com.winterproject.demo.AuthenticationResponse;
+import com.winterproject.demo.JwtUtil;
+import com.winterproject.demo.MyUserDetailService;
 import com.winterproject.demo.exception.LoginException;
+import com.winterproject.demo.note.Note;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -19,10 +29,42 @@ public class UserController {
     this.userService = userService;
   }
 
+  @Autowired
+  private AuthenticationManager authenticationManager;
+
+  @Autowired
+  private MyUserDetailService userDetailService;
+
+  @Autowired
+  private JwtUtil jwtTokenUtil;
+
   @GetMapping
   public List<User> getAllUser() {
 //    throw new ApiRequestException("Oops!");
     return userService.getAllUsers();
+  }
+
+  @GetMapping(path = "/hello")
+  public String hello() {
+    return "Hello";
+  }
+
+  @RequestMapping(value = "/authenticate", method = RequestMethod.POST)
+  public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
+
+    try {
+      this.authenticationManager.authenticate(
+              new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
+      );
+    } catch (BadCredentialsException e) {
+      throw new Exception("Incorrect username or password", e);
+    }
+
+    final UserDetails userDetails = userDetailService.loadUserByUsername(authenticationRequest.getUsername());
+
+    final String jwt = this.jwtTokenUtil.generateToken(userDetails);
+
+    return ResponseEntity.ok(new AuthenticationResponse(jwt));
   }
 
   // register a new user
